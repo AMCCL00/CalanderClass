f1e3aa6f59689a91845c75beae7ff9cbf5f99083
Andrew McClary <andrew.mccalry00@gmail.com>
2018-02-13 16:07:12 -0500

    Finished Project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class Main {
 
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+
+
     public static void main(String[] args) {
-	// write your code here
+
+        Main main = new Main();
+        main.getDate();
+    }
+
+    private void getDate() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_YEAR, 7);
+        System.out.println(dateFormat.format(calendar.getTime()));
     }
 }
